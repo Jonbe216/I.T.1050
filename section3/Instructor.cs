a12ab1f56e092f5599387af563f93dc83a5100cb
S00087738 <Jonbe216@gmail.com>
2018-11-26 18:07:27 -0500

    section 3

---
+++ section3/Instructor.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace section3
+{
+    public class Instructor
+    {
+
+        public string Name { get; set; }
+        
+        public string CourseName { get; set; }
+
+        public Instructor(string Name, string CourseName)
+        {
+            this.Name = Name;
+            this.CourseName = CourseName;
+        }
+
+        public void SetStudentGrade(Student Name, int Grade)
+        {
+            Name.Setgrade(Grade);
+        }
+
+        public void PrintInstructorInfo()
+        {
+            System.Console.WriteLine("Instructor: " + this.Name);
+            System.Console.WriteLine("Course Name: " + this.CourseName);
+            System.Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ section3/Student.cs
@@ -0,0 +1,40 @@
+﻿
+namespace section3
+{
+    public class Student
+    {
+        private int Grade;
+
+        public int GetGrade()
+        {
+            return this.Grade;
+        
+        }
+
+        public void Setgrade(int grade)
+        {
+            this.Grade = grade;
+        }
+
+        public string Name { get; set; }
+        public Instructor Teacher { get; set; }
+
+        public Student(string Name, Instructor Teacher)
+        {
+            this.Name = Name;
+            this.Teacher = Teacher;
+            this.Grade = 0;
+        }
+        
+        public void PrintStudentInfo()
+        {
+            System.Console.WriteLine("Student: " + this.Name);
+            System.Console.WriteLine("Grade: " + this.Grade);
+            Teacher.PrintInstructorInfo();
+            
+
+        }
+
+
+    }
+}
